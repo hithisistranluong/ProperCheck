a39a9f90be384db3e92367324626d93a4b67c7ec
Luong Ngoc Bao Tran <s4031723@rmit.edu.vn>
2024-11-13 10:15:42 +0700

    feat: add other types of Property basic constructors

--- src/CommercialProperty.java
+++ src/CommercialProperty.java
@@ -6,6 +6,72 @@ import java.util.*;
 
 public class CommercialProperty extends Property {
     private String businessType;
-    private boolean parkingSpaces;
+    private int parkingSpaces;
     private double squareFootage;
+
+    // Define Constructors
+    public CommercialProperty() {
+        super();
+        this.businessType = "Commercial";
+        this.parkingSpaces = 0;
+        this.squareFootage = 0.0;
+    }
+
+    public CommercialProperty(String id, String address, double pricing, String status, Owner ownerId) {
+        super(id, address, pricing, status, ownerId);
+        this.businessType = "Commercial";
+        this.parkingSpaces = 0;
+        this.squareFootage = 0.0;
+    }
+
+    public CommercialProperty(String id, String address, double pricing, String status, Owner owner,
+                              String businessType, int parkingSpaces, double squareFootage) {
+        super(id, address, pricing, status, owner);
+        this.businessType = businessType;
+        this.parkingSpaces = parkingSpaces;
+        this.squareFootage = squareFootage;
+    }
+
+    // Getters and setters
+    public String getBusinessType() {
+        return businessType;
+    }
+    public void setBusinessType(String businessType) {
+        this.businessType = businessType;
+    }
+
+    public int getParkingSpaces() {
+        return parkingSpaces;
+    }
+    public void setParkingSpaces(int parkingSpaces) {
+        this.parkingSpaces = parkingSpaces;
+    }
+
+    public double getSquareFootage() {
+        return squareFootage;
+    }
+    public void setSquareFootage(double squareFootage) {
+        this.squareFootage = squareFootage;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Commercial Property {" + "\n" +
+                "\tId: " + getPropertyId() + "\n" +
+                "\tAddress: " + getAddress() + "\n" +
+                "\tPricing: " + getPrice() + "\n" +
+                "\tStatus: " + getStatus() + " \n";
+
+        if (getOwner() != null) {
+            result += "\tOwner: " + getOwner().fullName + " - id: " + getOwner().id + "\n";
+        }
+
+        result += "\tHosts: " + getHosts() + "\n" +
+                "\tBussiness Type: " + getBusinessType() + "\n" +
+                "\tParking Spaces: " + getParkingSpaces() + "\n" +
+                "\tSquare Footage: " + getSquareFootage() + "\n" +
+                "}";
+
+        return result;
+    }
 }
--- src/ResidentialProperty.java
+++ src/ResidentialProperty.java
@@ -9,5 +9,69 @@ public class ResidentialProperty extends Property {
     private boolean hasGarden;
     private boolean petFriendly;
 
+    // Define Constructors
+    public ResidentialProperty() {
+        super();
+        bedrooms = 0;
+        hasGarden = false;
+        petFriendly = false;
+    }
 
+    public ResidentialProperty(String id, String address, double pricing, String status, Owner ownerId) {
+        super(id, address, pricing, status, ownerId);
+        bedrooms = 0;
+        hasGarden = false;
+        petFriendly = false;
+    }
+
+    public ResidentialProperty(String id, String address, double pricing, String status, Owner ownerId,
+                               int bedrooms, boolean hasGarden, boolean petFriendly) {
+        super(id, address, pricing, status, ownerId);
+        this.bedrooms = bedrooms;
+        this.hasGarden = hasGarden;
+        this.petFriendly = petFriendly;
+    }
+
+    // Getters and setters
+    public int getBedrooms() {
+        return bedrooms;
+    }
+    public void setBedrooms(int bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public boolean hasGarden() {
+        return hasGarden;
+    }
+    public void setHasGarden(boolean hasGarden) {
+        this.hasGarden = hasGarden;
+    }
+
+    public boolean petFriendly() {
+        return petFriendly;
+    }
+    public void setPetFriendly(boolean petFriendly) {
+        this.petFriendly = petFriendly;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Residential Property {" + "\n" +
+                        "\tId: " + getPropertyId() + "\n" +
+                        "\tAddress: " + getAddress() + "\n" +
+                        "\tPricing: " + getPrice() + "\n" +
+                        "\tStatus: " + getStatus() + " \n";
+
+        if (getOwner() != null) {
+            result += "\tOwner: " + getOwner().fullName + " - id: " + getOwner().id + "\n";
+        }
+
+        result += "\tHosts: " + getHosts() + "\n" +
+                  "\tBedrooms: " + getBedrooms() + "\n" +
+                  "\tHas Garden: " + hasGarden() + "\n" +
+                  "\tPet Friendly: " + petFriendly() + "\n" +
+                  "}";
+
+        return result;
+    }
 }
