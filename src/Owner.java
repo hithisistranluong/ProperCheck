a6b39c9241cc4c2a953fe0acd99abafd9544bfe2
Luong Ngoc Bao Tran <s4031723@rmit.edu.vn>
2024-11-13 09:35:07 +0700

    feat: add owner class and its basic constructors

---
+++ src/Owner.java
@@ -0,0 +1,39 @@
+/**
+ * @author Luong Ngoc Bao Tran - s4031723
+ */
+
+import java.util.*;
+
+public class Owner extends Person {
+    private List<Property> ownedProperties;
+    private List<Host> hosts;
+    private List<RentalAgreement> rentalAgreements;
+
+    // Define Constructors
+    public Owner() {
+        super();
+        this.ownedProperties = new ArrayList<Property>();
+        this.hosts = new ArrayList<Host>();
+        this.rentalAgreements = new ArrayList<RentalAgreement>();
+    }
+
+    public Owner(String id, String fullName, Date dob, String contact) {
+        super(id, fullName, dob, contact);
+        this.ownedProperties = new ArrayList<Property>();
+        this.hosts = new ArrayList<Host>();
+        this.rentalAgreements = new ArrayList<RentalAgreement>();
+    }
+
+    // Getters and setters
+    public List<Property> getOwnedProperties() {
+        return ownedProperties;
+    }
+
+    public List<Host> getHosts() {
+        return hosts;
+    }
+
+    public List<RentalAgreement> getRentalAgreements() {
+        return rentalAgreements;
+    }
+}
