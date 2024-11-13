40c84c260c9757622d17d0aba9c8775dfe084cdd
Luong Ngoc Bao Tran <s4031723@rmit.edu.vn>
2024-11-13 09:28:24 +0700

    feat: add basic constructors for tenant

--- src/Tenant.java
+++ src/Tenant.java
@@ -5,7 +5,44 @@
 import java.util.*;
 
 public class Tenant extends Person {
-    private RentalAgreement[] rentalAgreements;
-    private Payment[] paymentRecords;
-    private Property[] properties;
+    private List<RentalAgreement> rentalAgreements;
+    private List<Payment> paymentRecords;
+    private List<Property> rentalProperties;
+
+    // Define Constructors
+    public Tenant() {
+        super();
+        this.rentalAgreements = new ArrayList<RentalAgreement>();
+        this.paymentRecords = new ArrayList<Payment>();
+        this.rentalProperties = new ArrayList<Property>();
+    }
+
+    public Tenant(String id, String fullName, Date dob, String contact) {
+        super(id, fullName, dob, contact);
+        this.rentalAgreements = new ArrayList<RentalAgreement>();
+        this.paymentRecords = new ArrayList<Payment>();
+        this.rentalProperties = new ArrayList<Property>();
+    }
+
+    // Getters and setters
+    public List<RentalAgreement> getRentalAgreements() {
+        return rentalAgreements;
+    }
+//    public void addRentalAgreement(RentalAgreement agreement) {
+//        rentalAgreements.add(agreement);
+//    }
+
+    public List<Payment> getPaymentTransactions() {
+        return paymentRecords;
+    }
+//    public void addPayment(Payment payment) {
+//        paymentRecords.add(payment);
+//    }
+
+    public List<Property> getRentalProperties() {
+        return rentalProperties;
+    }
+//    public void addRentalProperty(Property property) {
+//        rentalProperties.add(property);
+//    }
 }
