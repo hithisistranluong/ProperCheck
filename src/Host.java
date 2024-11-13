c7ec4677ca850e7a9da4a03c13c3712844baa34d
Luong Ngoc Bao Tran <s4031723@rmit.edu.vn>
2024-11-13 09:30:13 +0700

    feat: add basic constructors for host

--- src/Host.java
+++ src/Host.java
@@ -5,7 +5,44 @@
 import java.util.*;
 
 public class Host extends Person {
-    private Property[] properties;
-    private Person[] cooperatingOwners;
-    private RentalAgreement[] rentalAgreements;
+    private List<Property> managedProperties;
+    private List<Person> cooperatingOwners;
+    private List<RentalAgreement> rentalAgreements;
+
+    // Define Constructors
+    public Host() {
+        super();
+        this.managedProperties = new ArrayList<Property>();
+        this.cooperatingOwners = new ArrayList<Person>();
+        this.rentalAgreements = new ArrayList<RentalAgreement>();
+    }
+
+    public Host(String id, String fullName, Date dob, String contact) {
+        super(id, fullName, dob, contact);
+        this.managedProperties = new ArrayList<Property>();
+        this.cooperatingOwners = new ArrayList<Person>();
+        this.rentalAgreements = new ArrayList<RentalAgreement>();
+    }
+
+    // Getters and setters
+    public List<Property> getManagedProperties() {
+        return managedProperties;
+    }
+//    public void addProperty(Property property) {
+//        managedProperties.add(property);
+//    }
+
+    public List<RentalAgreement> getRentalAgreements() {
+        return rentalAgreements;
+    }
+//    public void addCooperatingOwner(Person owner) {
+//        cooperatingOwners.add(owner);
+//    }
+
+    public List<Person> getCooperatingOwners() {
+        return cooperatingOwners;
+    }
+//    public void addRentalAgreement(RentalAgreement agreement) {
+//        rentalAgreements.add(agreement);
+//    }
 }
